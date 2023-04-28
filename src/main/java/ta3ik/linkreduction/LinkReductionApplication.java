e8503d72026dc940c8a62caa9bcbb8397a71a5df
Dmitriy <58227907+Ta3ikNSU@users.noreply.github.com>
2023-04-28 15:48:03 +0700

    init

---
+++ src/main/java/ta3ik/linkreduction/LinkReductionApplication.java
@@ -0,0 +1,13 @@
+package ta3ik.linkreduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkReductionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkReductionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ta3ik/linkreduction/LinkReductionApplicationTests.java
@@ -0,0 +1,13 @@
+package ta3ik.linkreduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkReductionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
